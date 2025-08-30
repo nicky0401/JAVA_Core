71edd5f1647a642d3c111f31913bf7e7ec9b9e93
이동혁 <nicky0401@naver.com>
2025-08-30 12:44:34 +0900

    초기설정

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,39 @@
+package hello.core.order;
+
+import hello.core.annotation.MainDisountPolicy;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+//@RequiredArgsConstructor 이용하면 생성자 대체 가능!
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    // 생성자가 딱 하나라 @AutoWired생략
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDisountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
+}
---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,15 @@
+package hello.core.order;
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.BeforeEach;
+
+class OrderServiceTest {
+    MemberService memberService;
+    OrderService orderService;
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
+}
